d14b4dc0437182027c26ec2c21a3b0068edbf937
diego <dqueiroz@quintoandar.com.br>
2015-03-05 11:54:59 -0300

    Devise for login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+      
+  root "queries#index"
+    
+  devise_for :users
   resources :queries
   
+  
   get 'queries/:id/exec' => 'queries#exec'
+  
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
